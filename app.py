a7c5437639e145296be4405f0bceadd3b884c786
SamPinder <sam.p25@k12.xrds.org>
2024-01-17 14:54:35 -0800

    Updated form confirmation page.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -18,5 +18,19 @@ def products():
 def contact():
     return render_template("contact.html")
 
+@app.route('/confirmation')
+def confirmation():
+    name = request.args.get("name")
+    address = request.args.get("address")
+    phone = request.args.get("phone")
+    email = request.args.get("email")
+    props = {
+        "name": name,
+        "address": address,
+        "phone": phone,
+        "email": email
+    }
+    return render_template("confirmation.html", data = props)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
